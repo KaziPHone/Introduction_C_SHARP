202a0afc2011c9987acdf3b77bdc40f0f508ad0d
Yury <58485439+KaziPHone@users.noreply.github.com>
2021-01-17 22:22:00 +0300

    Lesson_1

---
+++ ConsoleApplication2/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+
+namespace ConsoleApplication2
+{
+    internal class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.Write("Привет, как тебя зовут? ");
+            var name = Console.ReadLine();
+            Console.WriteLine($"Привет, {name}, сегодня {DateTime.Now.ToShortDateString()}!");
+
+            Console.ReadKey();
+        }
+    }
+}
